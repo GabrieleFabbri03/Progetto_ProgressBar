0a79f3fe46f356a93d9a5572624a1ea64fb76236
VioletHideaki <gabriele.fabbri@edu.unifi.it>
2025-11-17 15:24:40 +0000

    Test della classe FileLoader

--- FileLoader.cpp
+++ FileLoader.cpp
@@ -6,21 +6,32 @@
 FileLoader::FileLoader (const fs::path& path) : filePath(path) {}
 
 void FileLoader::load() {
-    if (!fs::exists(filePath)) {                                   //verifico se il percorso e' valido
+    try {
+        auto fileSize= fs::file_size(filePath);            //ottengo la dimensione del file in byte
+
+
+        size_t stepSize = std::max(static_cast<size_t>(1), fileSize / 100);
+        float stepMs = (fileSize / 1024)* 5 / 100;
+
+        bytesLoaded += stepSize;
+
+        if (fileSize == 0) {
+            done = true;
+            notifyObserver(100.0f);
+            return;
+        }
+
+        if (bytesLoaded >= fileSize) {
+            bytesLoaded = fileSize;
+            done = true;
+        }
+        //simulazione caricamento
+        float progress = static_cast<float>(bytesLoaded/fileSize)* 100.0f;
+
+        notifyObserver(progress); //aggiorno la barra di progresso
+        std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(stepMs))); //simulo il tempo di caricamento
+    }
+    catch (const std::filesystem::filesystem_error&) {                                   //verifico se il percorso e' valido
         std::cerr << "Errore: Percorso non valido!\n";
     }
-    auto fileSize= fs::file_size(filePath);                //ottengo la dimensione del file in byte
-    size_t stepSize = fileSize / 100;
-    float stepMs = (fileSize / 1024)* 5 / 100;
-
-    bytesLoaded += stepSize;
-    if (bytesLoaded >= fileSize) {
-        bytesLoaded = fileSize;
-        done = true;
-    }
-    //simulazione caricamento
-    float progress = static_cast<float>(bytesLoaded/fileSize)* 100.0f;
-
-    notifyObserver(progress); //aggiorno la barra di progresso
-    std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(stepMs))); //simulo il tempo di caricamento
 }
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,14 +1,14 @@
 #include "../googletest/googletest/include/gtest/gtest.h"
 #include "../ProgressBar.h"
 #include "../FileLoader.h"
-
+#include <fstream>
 
 //testo se la barra inizia da 0
 TEST(progressBar, initialValueZero) {
     ProgressBar progressBar(300.f, sf::Color::Black, sf::Color::Green);
     EXPECT_FLOAT_EQ(progressBar.getValue(), 0.0f);
 }
-//testo se la barra si aggiorna correttamente
+//testo se la barra si aggiorna correttamente e dimensioni
 TEST(progressBar, updateProgress) {
     ProgressBar progressBar(300.f, sf::Color::Black, sf::Color::Green);
     progressBar.updateProgress(50.0f);
@@ -18,6 +18,7 @@ TEST(progressBar, updateProgress) {
     EXPECT_FLOAT_EQ(size.x, 150.0f); //50% di 300
     EXPECT_FLOAT_EQ(size.y, 20.0f);  //h fissa
 }
+//verifica che posizione venga settata correttamente
 TEST(ProgressBarTest, SetPositionSetsBackgroundAndForeground) {
     ProgressBar bar(100.0f, sf::Color::Black, sf::Color::Green);
     bar.setPosition(10.0f, 20.0f);
@@ -29,4 +30,57 @@ TEST(ProgressBarTest, SetPositionSetsBackgroundAndForeground) {
     EXPECT_FLOAT_EQ(bgPos.y, 20.0f);
     EXPECT_FLOAT_EQ(fgPos.x, 10.0f);
     EXPECT_FLOAT_EQ(fgPos.y, 20.0f);
+}
+
+//test caricamento di un file inesistente
+TEST(FileLoader, LoadNonExistentFile) {
+    fs::path invalidPath("non_existent_file.txt");
+    FileLoader loader(invalidPath);
+
+    testing::internal::CaptureStderr(); //cattura l'output di errore
+    loader.load();
+    std::string output = testing::internal::GetCapturedStderr(); //restituisce in output catturato
+
+    EXPECT_NE(output.find("Errore: Percorso non valido!"), std::string::npos); //verifica che il messaggio di errore sia presente
+}
+
+//Caricamento file vuoto
+TEST(FileLoaderTest, LoadEmptyFile) {
+    std::ofstream out("empty_file.txt");
+    out.close();
+
+    FileLoader loader("empty_file.txt");
+
+    testing::internal::CaptureStderr();
+    loader.load();
+    std::string output = testing::internal::GetCapturedStderr();
+
+    EXPECT_EQ(output, "");
+    EXPECT_TRUE(loader.isDone());
+
+    std::remove("empty_file.txt");
+}
+
+//Caricamento file esistente
+TEST(FileLoaderTest, LoadExistingFile) {
+    // Creo un file temporaneo
+    std::ofstream out("temp_file.txt");
+    out << "Hello world";
+    out.close();
+
+    FileLoader loader("temp_file.txt");
+
+    // Catturo eventuali messaggi di errore
+    testing::internal::CaptureStderr();
+    while (!loader.isDone()) {
+        loader.load();
+    }
+    EXPECT_TRUE(loader.isDone());
+
+    std::string output = testing::internal::GetCapturedStderr();
+
+    EXPECT_EQ(output, "");      // nessun errore
+    EXPECT_TRUE(loader.isDone()); // done dovrebbe essere true
+
+    std::remove("temp_file.txt"); // pulizia
 }
