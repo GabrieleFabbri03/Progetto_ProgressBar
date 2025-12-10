f48d1ea2c69dd4094de373ad09ca3200dd65b25e
VioletHideaki <gabriele.fabbri@edu.unifi.it>
2025-12-10 12:12:49 +0000

    Commit finale

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -5,31 +5,31 @@
 
 //testo se la barra inizia da 0
 TEST(progressBar, initialValueZero) {
-    ProgressBar progressBar(300.f, sf::Color::Black, sf::Color::Green);
-    EXPECT_FLOAT_EQ(progressBar.getValue(), 0.0f);
+    ProgressBar progressBar(300.0, sf::Color::Black, sf::Color::Green);
+    EXPECT_FLOAT_EQ(progressBar.getValue(), 0.0);
 }
 //testo se la barra si aggiorna correttamente e dimensioni
 TEST(progressBar, updateProgress) {
-    ProgressBar progressBar(300.f, sf::Color::Black, sf::Color::Green);
-    progressBar.updateProgress(50.0f);
-    EXPECT_FLOAT_EQ(progressBar.getValue(), 50.0f);
+    ProgressBar progressBar(300.0, sf::Color::Black, sf::Color::Green);
+    progressBar.updateProgress(50.0);
+    EXPECT_FLOAT_EQ(progressBar.getValue(), 50.0);
 
     sf::Vector2f size = progressBar.getForegroundSize();
-    EXPECT_FLOAT_EQ(size.x, 150.0f); //50% di 300
-    EXPECT_FLOAT_EQ(size.y, 20.0f);  //h fissa
+    EXPECT_FLOAT_EQ(size.x, 150.0); //50% di 300
+    EXPECT_FLOAT_EQ(size.y, 20.0);  //h fissa
 }
 //verifica che posizione venga settata correttamente
 TEST(ProgressBarTest, SetPositionSetsBackgroundAndForeground) {
-    ProgressBar bar(100.0f, sf::Color::Black, sf::Color::Green);
-    bar.setPosition(10.0f, 20.0f);
+    ProgressBar bar(100.0, sf::Color::Black, sf::Color::Green);
+    bar.setPosition(10.0, 20.0);
 
     sf::Vector2f bgPos = bar.getBackgroundPosition();
     sf::Vector2f fgPos = bar.getForegroundPosition();
 
-    EXPECT_FLOAT_EQ(bgPos.x, 10.0f);
-    EXPECT_FLOAT_EQ(bgPos.y, 20.0f);
-    EXPECT_FLOAT_EQ(fgPos.x, 10.0f);
-    EXPECT_FLOAT_EQ(fgPos.y, 20.0f);
+    EXPECT_FLOAT_EQ(bgPos.x, 10.0);
+    EXPECT_FLOAT_EQ(bgPos.y, 20.0);
+    EXPECT_FLOAT_EQ(fgPos.x, 10.0);
+    EXPECT_FLOAT_EQ(fgPos.y, 20.0);
 }
 
 //test caricamento di un file inesistente
